d90be1ad1f03da4d0563ae75be11560211465e39
tejaram15 <raghuramkarra15@gmail.com>
2017-07-05 11:46:11 +0530

    Update login and Signup Feature

--- Event Driven Framework/HTTP_SERVER.js
+++ Event Driven Framework/HTTP_SERVER.js
@@ -1,7 +1,8 @@
-var path = require('path');
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const exec = require('child_process').exec;
 
 var dir = path.join(__dirname, 'public');
 
@@ -17,54 +18,77 @@ var mime = {
 };
 
 var server = http.createServer(function (req, res) {
-    var q = url.parse(req.url,true);
-    var filename = "."+q.pathname;
-    var type = mime[path.extname(filename).slice(1)] || 'text/plain';
-    fs.readFile(filename,function (err,data){
-        if(err){
-            res.writeHead(404,{'Content-Type':'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200,{'Content-Type':type});
-        res.write(data);
-        return res.end();
-    });
+    if(req.method == "POST"){
+        console.log("POST");
+        var body = '';
+        var result = '';
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        req.on('data', function (data) {
+            body += data;
+            //Show for Debugging
+            //console.log("Body: " + body);
+            var str = body;
+            if (str.charAt(0) === '"' && str.charAt(str.length -1) === '"')
+            {
+                //console.log(str.substr(1,str.length -2));
+                body = str.substr(1,str.length -2);
+            }            
+            result = body.split(",");
+            console.log(result);
+            if(result[0]=="signup"){
+                var username = result[1];
+                var pass = result[2];
+                var check = "curl localhost:9090/query -sS -XPOST -d '" + "{me(func:allofterms(username,\"" + username + "\")){username _uid_}" + "}\'";
+                exec(check, function(error, stdout, stderr) {
+                    console.log(stdout);
+                    if(stdout=="{}"){
+                        console.log("Ready to Signup!!");
+                        var query = "curl localhost:9090/query -sS -XPOST -d '";
+                        query += "mutation{set{_:" + username + " <username> \"" + username + "\" .";
+                        query += "_:" + username + " <password> \"" + pass + "\" .";
+                        query += "}schema{username: string @index(term) .}}'";
+                        //console.log(query);
+                        exec(query, function(error, stdout, stderr) {
+                          // command output is in stdout
+                            console.log(stdout);
+                            var obj = JSON.parse(stdout);
+                            if(obj["code"]=="Success")
+                                res.end("Signup Successful");
+                        });                                        
+                    }
+                    else res.end("User Already Exists");
+                });
+            }
+            else if(result[0]=="login"){
+                var username = result[1];
+                var pass = result[2];
+                var query = "curl localhost:9090/query -sS -XPOST -d '" + "{log(func:allofterms(username,\"" + username + "\")){username password}" + "}\'";
+                console.log(query);
+                exec(query, function(error, stdout, stderr) {
+                  // command output is in stdout
+                    console.log(stdout);
+                    if(stdout["code"]=="Success")
+                        res.end("Login Successful");
+                });                
+            }            
+        });        
+    }
+    else{
+        var q = url.parse(req.url,true);
+        var filename = "."+q.pathname;
+        var type = mime[path.extname(filename).slice(1)] || 'text/plain';
+        fs.readFile(filename,function (err,data){
+            if(err){
+                res.writeHead(404,{'Content-Type':'text/html'});
+                return res.end("404 Not Found");
+            }
+            res.writeHead(200,{'Content-Type':type});
+            res.write(data);
+            return res.end();
+        });
+    }
 });
 
 server.listen(8080, function () {
     console.log('Listening on http://localhost:8080/');
 });
-
-/*var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-console.log('HTTP Server Started');
-http.createServer(function(req, res){
-    console.log(req.url);
-    var ctype;
-    switch(req.url)
-    {
-        case "/logo%20(1).png":
-            console.log("Fetching Photo");
-            ctype = "img/png";
-            break;
-        case "/w3.css":
-            ctype = "text/css";
-            break;
-        default:
-            ctype = "text/html";
-    }
-    var q = url.parse(req.url,true);
-    var filename = "."+q.pathname;
-    fs.readFile(filename,function (err,data){
-        if(err){
-            res.writeHead(404,{'Content-Type':'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200,{'Content-Type':ctype});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
-*/
